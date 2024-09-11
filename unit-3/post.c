d95b0554ace54d8557b1937ed50196254340a0e0
Konseptt <sharmaranjan816@gmail.com>
2024-09-11 17:36:32 +0000

    postfix

--- unit-3/post.c
+++ unit-3/post.c
@@ -1,36 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 
-#define MAX 100
+#define MAX_SIZE 100
 
-// Function to evaluate postfix expression
-int evaluatePostfix(char* exp) {
-    int stack[MAX];
+// Function to check if a character is an operator
+int isOperator(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
+}
+
+// Function to get the precedence of an operator
+int getPrecedence(char ch) {
+    if (ch == '^') {
+        return 3;
+    } else if (ch == '*' || ch == '/') {
+        return 2;
+    } else if (ch == '+' || ch == '-') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Function to convert infix to postfix
+void infixToPostfix(char infix[], char postfix[]) {
+    int i, j = 0;
+    char stack[MAX_SIZE];
     int top = -1;
 
-    for (int i = 0; exp[i]; ++i) {
-        if (isdigit(exp[i])) {
-            stack[++top] = exp[i] - '0';
-        } else {
-            int val1 = stack[top--];
-            int val2 = stack[top--];
-            switch (exp[i]) {
-                case '+': stack[++top] = val2 + val1; break;
-                case '-': stack[++top] = val2 - val1; break;
-                case '*': stack[++top] = val2 * val1; break;
-                case '/': stack[++top] = val2 / val1; break;
+    for (i = 0; infix[i] != '\0'; i++) {
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        } else if (infix[i] == '(') {
+            stack[++top] = infix[i];
+        } else if (infix[i] == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[j++] = stack[top--];
             }
+            if (top == -1) {
+                printf("Invalid expression\n");
+                return;
+            }
+            top--; // Pop '('
+        } else if (isOperator(infix[i])) {
+            while (top != -1 && getPrecedence(infix[i]) <= getPrecedence(stack[top])) {
+                postfix[j++] = stack[top--];
+            }
+            stack[++top] = infix[i];
         }
     }
-    return stack[top];
+
+    while (top != -1) {
+        postfix[j++] = stack[top--];
+    }
+    postfix[j] = '\0';
 }
 
 int main() {
-    char exp[MAX];
-    printf("Enter postfix expression: ");
-    scanf("%s", exp);
-    printf("Result of postfix evaluation: %d\n", evaluatePostfix(exp));
+    char infix[MAX_SIZE], postfix[MAX_SIZE];
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Infix expression: %s\n", infix);
+    printf("Postfix expression: %s\n", postfix);
+
     return 0;
 }
